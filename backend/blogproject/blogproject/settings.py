2d437869b169b26203ffcfc7b8b08444fc1f0ee5
rashaeshaal <rashaeshaal@gmail.com>
2024-10-03 09:20:29 +0530

    changes done  in settings

--- backend/blogproject/blogproject/settings.py
+++ backend/blogproject/blogproject/settings.py
@@ -20,6 +20,7 @@ load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+print(BASE_DIR,'hiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii')
 
 
 # Quick-start development settings - unsuitable for production
@@ -166,7 +167,9 @@ AUTH_USER_MODEL = 'accounts.User'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
-
+STATICFILES_DIRS = [
+    BASE_DIR / 'media'
+]
 
 
 
